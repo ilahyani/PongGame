180b3511d9ce4efc70e975ca257ced8fc8c04a33
ilahyani <ilahyani@student.1337.ma>
2023-05-14 17:02:10 +0100

    center ball

--- main.js
+++ main.js
@@ -5,17 +5,17 @@ const ball = document.querySelector(".ball")
 let start, animationID, progress, done = false
 
 console.log(document.body.clientWidth)
-let paddin = 48 + 2 + parseInt(window.getComputedStyle(canvas, null).getPropertyValue('padding-left')) + parseInt(player1.clientWidth)
+let paddin = 52 + parseInt(window.getComputedStyle(canvas, null).getPropertyValue('padding-left')) + parseInt(player1.clientWidth) + 2
 function move(timestamp) {
-    if (!start) start = timestamp
+    if (!start)
+        start = timestamp
     progress = timestamp - start
+    console.log(progress, timestamp, start)
     if (progress >= document.body.clientWidth - paddin) {
         progress = document.body.clientWidth - paddin
         console.log("progress:", progress)
     }
     ball.style.left = progress + "px"
-    console.log(parseInt(ball.style.left))
-    // ball.style.bottom = (progress / -6) + "px"
     if (parseInt(ball.style.left) === document.body.clientWidth - paddin) {
         cancelAnimationFrame(animationID)
         console.log("animation done", parseInt(ball.style.left), document.body.clientWidth - paddin)
