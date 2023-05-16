06aab9d3753c35e55f297b1a48135655a78e130c
ilahyani <ilahyani@student.1337.ma>
2023-05-16 13:48:08 +0100

    canvas setup

--- main.js
+++ main.js
@@ -1,28 +1,40 @@
 const player = document.querySelector(".player")
-const score = document.querySelector(".score")
-const main = document.querySelector(".main")
 const ball = document.querySelector(".ball")
+const score = document.querySelector(".score")
 
-document.addEventListener('keydown', function (event) {
-    if (event.key === "ArrowRight") {
-        console.log("right")
+// document.addEventListener('keydown', function (event) {
+    //     if (event.key === "ArrowRight") {
+        //         console.log("->")
+        //     }
+        //     else if (event.key === "ArrowLeft") {
+            //         console.log("<-")
+            //     }
+            // })
+            
+            // let ballAnimationID, playerAnimationID
+            
+            // let maxRight = canvas.clientWidth - player.clientWidth
+            // console.log(player)
+            // function movePlayer() {
+                //     playerAnimationID = window.requestAnimationFrame(movePlayer)
+                // }
+                // // window.requestAnimationFrame(movePlayer)
+                
+                // console.log(ball)
+                // function moveBall() {
+//     ballAnimationID = window.requestAnimationFrame(moveBall)
+// }
+// // window.requestAnimationFrame(moveBall)
+
+window.addEventListener("load", () => {
+    console.log("PAGE LOADED")
+    draw()
+});
+
+function draw() {
+    console.log("DRAW STARTED")
+    const canvas = document.querySelector(".playground")
+    if (canvas.getContext) {
+        const ctx = canvas.getContext("2d")
     }
-    else if (event.key === "ArrowLeft") {
-        console.log("left")
-    }
-})
-
-let ballAnimationID, playerAnimationID
-
-let maxRight = main.clientWidth - player.clientWidth
-console.log(player)
-function movePlayer() {
-    playerAnimationID = window.requestAnimationFrame(movePlayer)
 }
-// window.requestAnimationFrame(movePlayer)
-
-console.log(ball)
-function moveBall() {
-    ballAnimationID = window.requestAnimationFrame(moveBall)
-}
-// window.requestAnimationFrame(moveBall)
