f100effe4bd6fbfd649c84a0bd0a9aa1633b5fc2
ilahyani <lahyani799@gmail.com>
2023-05-20 12:11:41 +0100

    ball movement done

--- main.js
+++ main.js
@@ -2,34 +2,30 @@ const PLAYER_WIDTH = 250, PLAYER_HEIGHT = 16, BALL_RADIUS = 12
 const canvas = document.querySelector(".playground")
 const ctx = canvas.getContext("2d")
 const player = {
-    player: new Path2D(),
     x: canvas.width / 2 - PLAYER_WIDTH / 2,
     y: canvas.height - PLAYER_HEIGHT,
+    v: 8
 }
 const ball = {
-    ball: new Path2D(),
     x: canvas.width / 2 - BALL_RADIUS / 2,
-    y: canvas.height / 2
+    y: canvas.height / 2,
+    vy: 5,
+    vx: 5
 }
 
-let velocity = 6
 window.addEventListener("load", () => {
     console.log("PAGE LOADED")
     init()
 });
 document.addEventListener('keydown', function (event) {
     if (event.key === "ArrowRight") {
-        // console.log("->")
-        // console.log(player.x)
         ctx.clearRect(player.x, player.y - 10, PLAYER_WIDTH, PLAYER_HEIGHT)
-        player.x = Math.min(canvas.width - PLAYER_WIDTH, player.x + velocity)
+        player.x = Math.min(canvas.width - PLAYER_WIDTH, player.x + player.v)
         ctx.fillRect(player.x, player.y - 10, PLAYER_WIDTH, PLAYER_HEIGHT)
     }
     else if (event.key === "ArrowLeft") {
-        // console.log("<-")
-        // console.log(player.x)
         ctx.clearRect(player.x, player.y - 10, PLAYER_WIDTH, PLAYER_HEIGHT)
-        player.x = Math.max(0, player.x - velocity)
+        player.x = Math.max(0, player.x - player.v)
         ctx.fillRect(player.x, player.y - 10, PLAYER_WIDTH, PLAYER_HEIGHT)
     }
 })
@@ -37,21 +33,28 @@ document.addEventListener('keydown', function (event) {
 function init() {
     ctx.fillStyle = "white"
     //DRAW PLAYER
-    player.player.rect(player.x, player.y - 10, PLAYER_WIDTH, PLAYER_HEIGHT)
+    ctx.rect(player.x, player.y - 10, PLAYER_WIDTH, PLAYER_HEIGHT)
     ctx.fill(player.player)
     //DRAW BALL
-    ball.ball.arc(ball.x, ball.y, BALL_RADIUS, 0, 2 * Math.PI)
-    ctx.fill(ball.ball)
+    ctx.arc(ball.x, ball.y, BALL_RADIUS, 0, 2 * Math.PI)
+    ctx.fill()
 }
 
 let ballAnimationID
 function moveBall() {
+    ctx.clearRect(ball.x - BALL_RADIUS, ball.y - BALL_RADIUS, BALL_RADIUS * 2, BALL_RADIUS * 2)
+    // ctx.clearRect(0, 0, canvas.width, canvas.height)
+    // ctx.fillRect(player.x, player.y - 10, PLAYER_WIDTH, PLAYER_HEIGHT)
+    if ((ball.x + BALL_RADIUS) + ball.vx > canvas.width || (ball.x - BALL_RADIUS) + ball.vx < 0)
+        ball.vx *= -1
+    if ((ball.y + BALL_RADIUS) + ball.vy > canvas.height || (ball.y - BALL_RADIUS) + ball.vy < 0)
+        ball.vy *= -1
+    ball.x += ball.vx
+    ball.y += ball.vy
+    ctx.beginPath()
+    ctx.arc(ball.x, ball.y, BALL_RADIUS, 0, 2 * Math.PI)
+    ctx.fill()
+    ctx.closePath()
     ballAnimationID = window.requestAnimationFrame(moveBall)
 }
-// window.requestAnimationFrame(moveBall)
-
-// let playerAnimationID
-// function movePlayer() {
-//     playerAnimationID = window.requestAnimationFrame(movePlayer)
-// }
-// window.requestAnimationFrame(movePlayer)
+window.requestAnimationFrame(moveBall)
