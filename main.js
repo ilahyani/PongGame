cdae8152d25883490e66041cab39a8f3a9222e05
ilahyani <lahyani799@gmail.com>
2023-05-20 16:33:31 +0100

    added player collision

--- main.js
+++ main.js
@@ -4,13 +4,13 @@ const ctx = canvas.getContext("2d")
 const player = {
     x: canvas.width / 2 - PLAYER_WIDTH / 2,
     y: canvas.height - PLAYER_HEIGHT,
-    v: 8
+    v: 9
 }
 const ball = {
     x: canvas.width / 2 - BALL_RADIUS / 2,
     y: canvas.height / 2,
-    vy: 5,
-    vx: 5
+    vy: 4,
+    vx: 4
 }
 
 window.addEventListener("load", () => {
@@ -32,23 +32,28 @@ document.addEventListener('keydown', function (event) {
 
 function init() {
     ctx.fillStyle = "white"
-    //DRAW PLAYER
     ctx.rect(player.x, player.y - 10, PLAYER_WIDTH, PLAYER_HEIGHT)
     ctx.fill(player.player)
-    //DRAW BALL
     ctx.arc(ball.x, ball.y, BALL_RADIUS, 0, 2 * Math.PI)
     ctx.fill()
 }
 
 let ballAnimationID
 function moveBall() {
-    ctx.clearRect(ball.x - BALL_RADIUS, ball.y - BALL_RADIUS, BALL_RADIUS * 2, BALL_RADIUS * 2)
-    // ctx.clearRect(0, 0, canvas.width, canvas.height)
-    // ctx.fillRect(player.x, player.y - 10, PLAYER_WIDTH, PLAYER_HEIGHT)
-    if ((ball.x + BALL_RADIUS) + ball.vx > canvas.width || (ball.x - BALL_RADIUS) + ball.vx < 0)
-        ball.vx *= -1
-    if ((ball.y + BALL_RADIUS) + ball.vy > canvas.height || (ball.y - BALL_RADIUS) + ball.vy < 0)
+    // ctx.clearRect(ball.x - BALL_RADIUS, ball.y - BALL_RADIUS, BALL_RADIUS * 2, BALL_RADIUS * 2)
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    ctx.fillRect(player.x, player.y - 10, PLAYER_WIDTH, PLAYER_HEIGHT)
+    if ((ball.y + BALL_RADIUS) >= player.y - 13 && ball.x >= player.x && ball.x <= player.x + PLAYER_WIDTH)
         ball.vy *= -1
+    else if ((ball.x + BALL_RADIUS) + ball.vx > canvas.width || (ball.x - BALL_RADIUS) + ball.vx < 0)
+        ball.vx *= -1
+    else if ((ball.y - BALL_RADIUS) + ball.vy < 0)
+        ball.vy *= -1
+    else if ((ball.y + BALL_RADIUS) + ball.vy > canvas.height) {
+        ball.vx = ball.vy = 4
+        ball.x = (canvas.width / 2 - BALL_RADIUS / 2) - ball.vx
+        ball.y = canvas.height / 2 - ball.vy
+    }
     ball.x += ball.vx
     ball.y += ball.vy
     ctx.beginPath()
